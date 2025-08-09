d251202e6f5cd0d27dc891f6fb30a49165b41e72
Joe Rogers <frogers@uci.edu>
2025-08-08 18:16:47 -0700

    Add toolbar background color

--- Frontend/src/pages/Whiteboard.tsx
+++ Frontend/src/pages/Whiteboard.tsx
@@ -8,13 +8,13 @@ function Whiteboard() {
   return (
     <main className="flex flex-col justify-center"> {/* Might not need this to be flex anymore */}
       {/* Header | TODO: separate into Header component */}
-      <div className="fixed top-1 left-0 right-0 max-h-15 text-center shadow-md rounded-2xl mx-20 m-1 p-3"> 
-        <h1 className="text-2xl">{title}</h1>
+      <div className="fixed top-1 left-0 right-0 max-h-15 text-center shadow-md rounded-2xl mx-20 m-1 p-3 bg-stone-50"> 
+        <h1 className="text-2xl font-bold">{title}</h1>
       </div>
       {/* Content */}
       <div className="flex flex-1 justify-center items-center mt-20"> {/* Might not need this to be flex anymore */}
         {/* Toolbar */}
-        <aside className="fixed top-1/2 -translate-y-1/2 left-1 max-w-40 flex flex-col flex-shrink-0 text-center p-4 m-1 rounded-2xl shadow-md"> {/* TODO: separate into Sidebar component */}
+        <aside className="fixed top-1/2 -translate-y-1/2 left-2 max-w-40 flex flex-col flex-shrink-0 text-center p-4 m-1 rounded-2xl shadow-md  bg-stone-50"> {/* TODO: separate into Sidebar component */}
           <h2 className="text-2xl font-bold mb-4">Tools</h2>
           {tools.map((tool) => {
             return (
@@ -23,7 +23,7 @@ function Whiteboard() {
           })}
         </aside>
         {/* Canvas Container */}
-        <div className="flex flex-1 flex-row justify-center flex-wrap ml-42">
+        <div className="flex flex-1 flex-row justify-center flex-wrap ml-40">
           <CanvasCard title="Canvas A"></CanvasCard>
           <CanvasCard title="Canvas B"></CanvasCard>
           <CanvasCard title="Canvas C"></CanvasCard>
