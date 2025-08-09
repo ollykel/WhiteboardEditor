aa436f8358ead8e7683bb408522032e659efbcfd
Joe Rogers <frogers@uci.edu>
2025-08-08 18:09:49 -0700

    Fix header

--- Frontend/src/pages/Whiteboard.tsx
+++ Frontend/src/pages/Whiteboard.tsx
@@ -6,13 +6,13 @@ const title = "My First Whiteboard";
 
 function Whiteboard() {
   return (
-    <main className="flex flex-col justify-center">
+    <main className="flex flex-col justify-center"> {/* Might not need this to be flex anymore */}
       {/* Header | TODO: separate into Header component */}
-      <div className="text-center shadow-md rounded-2xl mx-20 m-1 p-3"> 
+      <div className="fixed top-1 left-0 right-0 max-h-15 text-center shadow-md rounded-2xl mx-20 m-1 p-3"> 
         <h1 className="text-2xl">{title}</h1>
       </div>
       {/* Content */}
-      <div className="flex flex-1 justify-center items-center"> {/* Might not need this to be flex anymore */}
+      <div className="flex flex-1 justify-center items-center mt-20"> {/* Might not need this to be flex anymore */}
         {/* Toolbar */}
         <aside className="fixed top-1/2 -translate-y-1/2 left-1 max-w-40 flex flex-col flex-shrink-0 text-center p-4 m-1 rounded-2xl shadow-md"> {/* TODO: separate into Sidebar component */}
           <h2 className="text-2xl font-bold mb-4">Tools</h2>
@@ -26,11 +26,10 @@ function Whiteboard() {
         <div className="flex flex-1 flex-row justify-center flex-wrap ml-42">
           <CanvasCard title="Canvas A"></CanvasCard>
           <CanvasCard title="Canvas B"></CanvasCard>
-          <CanvasCard title="Canvas A"></CanvasCard>
-          <CanvasCard title="Canvas B"></CanvasCard>
-          <CanvasCard title="Canvas A"></CanvasCard>
-          <CanvasCard title="Canvas B"></CanvasCard>
-          {/* <CanvasCard title="Canvas C"></CanvasCard> */}
+          <CanvasCard title="Canvas C"></CanvasCard>
+          <CanvasCard title="Canvas D"></CanvasCard>
+          <CanvasCard title="Canvas E"></CanvasCard>
+          <CanvasCard title="Canvas F"></CanvasCard>
         </div>
       </div>
     </main>
