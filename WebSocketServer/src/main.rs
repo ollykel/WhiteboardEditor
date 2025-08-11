4044d481df37548b4242c4a3bf39d74fb357998a
Oliver A. Kelton <oakelton@gmail.com>
2025-08-11 14:22:19 -0700

    Implemented Whiteboard, Canvas types in WebSocketServer

--- WebSocketServer/src/main.rs
+++ WebSocketServer/src/main.rs
@@ -15,8 +15,8 @@ enum ShapeModel {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct CanvasClientView {
     id: CanvasIdType,
-    width: usize,
-    height: usize,
+    width: u64,
+    height: u64,
     shapes: Vec<ShapeModel>,
 }
 
@@ -35,6 +35,48 @@ enum WebSocketMessage {
     ClientLogout { client_id: ClientIdType },
     CreateShape { client_id: ClientIdType, canvas_id: CanvasIdType, shape: ShapeModel },
     CreateCanvas { client_id: ClientIdType, width: u64, height: u64 },
+}
+
+#[derive(Clone)]
+struct Canvas {
+    id: CanvasIdType,
+    width: u64,
+    height: u64,
+    shapes: Vec<ShapeModel>,
+}
+
+impl Canvas {
+    pub fn to_client_view(&self) -> CanvasClientView {
+        // At the moment, the client view is identical to the Canvas type itself, but this may not
+        // always be the case.
+        CanvasClientView {
+            id: self.id,
+            width: self.width,
+            height: self.height,
+            shapes: self.shapes.clone(),
+        }
+    }// end pub fn to_client_view(&self) -> CanvasClientView
+}
+
+#[derive(Clone)]
+struct Whiteboard {
+    id: WhiteboardIdType,
+    name: String,
+    canvases: Vec<Canvas>,
+}
+
+impl Whiteboard {
+    pub fn to_client_view(&self) -> WhiteboardClientView {
+        // At the moment, the client view is identical to the Canvas type itself, but this may not
+        // always be the case.
+        WhiteboardClientView {
+            id: self.id,
+            name: self.name.clone(),
+            canvases: self.canvases.iter()
+                .map(|c| c.to_client_view())
+                .collect()
+        }
+    }// end pub fn to_client_view(&self) -> CanvasClientView
 }
 
 fn main() {
