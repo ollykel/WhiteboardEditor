6ac67d56ac93c15b7fb1351d518f3cf6c1dff97c
Oliver A. Kelton <oakelton@gmail.com>
2025-08-11 14:01:54 -0700

    Created server side version of message protocol in WebSocketServer/src/main.rs

--- WebSocketServer/src/main.rs
+++ WebSocketServer/src/main.rs
@@ -1,3 +1,41 @@
+use serde::{Deserialize, Serialize};
+
+type ClientIdType = i32;
+type CanvasIdType = i32;
+type WhiteboardIdType = i32;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum ShapeModel {
+    Rect { x: u64, y: u64, width: u64, height: u64 },
+    Ellipse { x: u64, y: u64, radiusX: u64, radiusY: u64 },
+    Vector { points: Vec<u64> },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct CanvasClientView {
+    id: CanvasIdType,
+    width: usize,
+    height: usize,
+    shapes: Vec<ShapeModel>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct WhiteboardClientView {
+    id: WhiteboardIdType,
+    name: String,
+    canvases: Vec<CanvasClientView>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case", rename_all_fields="camelCase")]
+enum WebSocketMessage {
+    InitClient { client_id: ClientIdType, active_clients: Vec<ClientIdType>, whiteboard: WhiteboardClientView },
+    ClientLogin { client_id: ClientIdType },
+    ClientLogout { client_id: ClientIdType },
+    CreateShape { client_id: ClientIdType, canvas_id: CanvasIdType, shape: ShapeModel },
+}
+
 fn main() {
     println!("Hello, world!");
 }
