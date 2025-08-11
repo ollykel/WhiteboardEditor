751f50d16db1280d9af34362e8b98a2bc304a693
Oliver A. Kelton <oakelton@gmail.com>
2025-08-11 16:17:24 -0700

    Implemented web socket connection funcionality in Whiteboard.tsx

--- Frontend/src/pages/Whiteboard.tsx
+++ Frontend/src/pages/Whiteboard.tsx
@@ -1,17 +1,54 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import CanvasCard from "@/components/CanvasCard";
 import Toolbar from "@/components/Toolbar";
 import Header from '@/components/Header';
 
 import type { ToolChoice } from '@/components/Tool';
+import type { SocketMessage } from '@/types/WebSocketProtocol';
 
 // TODO: Swap out hardcoded placeholders with dynamic data
 const title = "My First Whiteboard";
 
+const getWebSocketUri = (): string => {
+    const wsScheme = window.location.protocol === 'https:' ? 'wss' : 'ws';
+    const wsUri = `${wsScheme}://${window.location.host}/ws`;
+
+    return wsUri;
+};
+
 const Whiteboard = () => {
   const [toolChoice, setToolChoice] = useState<ToolChoice>('rect');
   const [canvases, setCanvases] = useState<{ id: number, title: string }[]>([{ id: 1, title: "Canvas A" }]);
+  const socketRef = useRef<WebSocket | null>(null);
+
+  // handles all web socket messages
+  const handleMessage = (event: any): void => {
+    try {
+      const msg = JSON.parse(event.data) as SocketMessage;
+      console.log('Received:', msg);
+
+      // TODO: handle each type of message
+    } catch (e) {
+      console.log('Failed to parse message:', e);
+    }
+  };
+
+  // Set up web socket connection
+  useEffect(() => {
+    const wsUri = getWebSocketUri();
+    const ws = new WebSocket(wsUri);
+
+    ws.onopen = () => {
+      console.log(`Established web socket connection to ${wsUri}`);
+      socketRef.current = ws;
+    };
+    ws.onerror = () => {
+      console.log(`Failed to establish web socket connection to ${wsUri}`);
+      socketRef.current = null;
+    };
+    ws.onmessage = handleMessage;
+  }, []);
 
   const nextID = useRef(2);
   const nextCanvasTitle = useRef(66);
