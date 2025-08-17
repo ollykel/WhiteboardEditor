4a9bfcc2d5ee6b19a7b9e6f70635ef168868828d
Joe Rogers <frogers@uci.edu>
2025-08-17 13:50:41 -0700

    Style whiteboard card, add image

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Dashboard from './pages/Dashboard';
 import Whiteboard from './pages/Whiteboard';
+import { Routes, Route, Link } from 'react-router';
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route />
+      </Routes>
       <Dashboard />
       {/* <Whiteboard /> */}
     </div>
--- Frontend/src/components/WhiteboardCard.tsx
+++ Frontend/src/components/WhiteboardCard.tsx
@@ -1,7 +1,7 @@
 function WhiteboardCard() {
   return (
-    <div className="flex flex-col justify-center m-10 max-w-30">
-      <img src="" alt="" />
+    <div className="flex flex-col justify-center m-10 p-5 w-50 rounded-xl shadow">
+      <img src="/images/Screenshot 2025-08-17 at 1.16.54 PM.png" alt="Whiteboard Thumbnail" />
       <h1 className=" text-lg font-bold">Title</h1>
       <h2 className="">Owner: </h2>
       <h3 className="">Collaborators: </h3>
