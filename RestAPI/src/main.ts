b7461c2d96321ad16b42c48e0c6dc4dbc9542712
Oliver A. Kelton <oakelton@gmail.com>
2025-08-21 16:19:19 -0700

    Implemented mongoose database connection in rest_api

--- RestAPI/src/main.ts
+++ RestAPI/src/main.ts
@@ -1,6 +1,15 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
+
+// Connect to MongoDB
+const MONGO_URI = process.env.MONGO_URI;
+
+if (! MONGO_URI) {
+  console.error('ERROR: environment variable MONGO_URI not set');
+  process.exit(1);
+}
 
 const app = express();
 
@@ -23,6 +32,15 @@ app.get(`/api/${API_VERSION}/health`, (_req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`REST API server listening on port ${PORT} ...`);
+mongoose.connect(MONGO_URI)
+.then(() => {
+  // Only start listening once the database connection has been established
+  console.log('Connected to database successfully');
+
+  app.listen(PORT, () => {
+    console.log(`REST API server listening on port ${PORT} ...`);
+  });
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
 });
