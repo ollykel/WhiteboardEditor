36c10c02292c21a50cfc4f9702946af04330d94e
Oliver A. Kelton <oakelton@gmail.com>
2025-08-28 17:06:00 -0700

    Added react-query QueryClient to App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,18 +1,24 @@
+import { Routes, Route } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import Dashboard from './pages/Dashboard';
 import Whiteboard from './pages/Whiteboard';
 import UserAuth from './pages/UserAuth';
-import { Routes, Route } from 'react-router';
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<UserAuth action="login"/>} />
-        <Route path="/signup" element={<UserAuth action="signup"/>} />
-        <Route path="/whiteboard/new" element={<Whiteboard />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<UserAuth action="login"/>} />
+          <Route path="/signup" element={<UserAuth action="signup"/>} />
+          <Route path="/whiteboard/new" element={<Whiteboard />} />
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 };
