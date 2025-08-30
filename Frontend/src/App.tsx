d3efac912e609304a95731a1b5c13d2f7e4eb6c8
Oliver A. Kelton <oakelton@gmail.com>
2025-08-30 09:56:12 -0700

    Implemented AccountSettings page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Dashboard from './pages/Dashboard';
 import Whiteboard from './pages/Whiteboard';
 import UserAuth from './pages/UserAuth';
+import AccountSettings from './pages/AccountSettings';
 import { AuthProvider } from './AuthContext';
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<UserAuth action="login"/>} />
             <Route path="/signup" element={<UserAuth action="signup"/>} />
+            <Route path="/account" element={<AccountSettings />} />
             <Route path="/whiteboard/new" element={<Whiteboard />} />
           </Routes>
         </QueryClientProvider>
