1c8fe421def46b00b3fba133f2852208d7b1b51c
Oliver A. Kelton <oakelton@gmail.com>
2025-08-31 22:10:40 -0700

    Moved useEllipseDispatcher to own file

--- Frontend/src/components/Canvas.tsx
+++ Frontend/src/components/Canvas.tsx
@@ -8,7 +8,7 @@
 // =============================================================================
 
 import { useState, useRef } from 'react';
-import { Stage, Layer, Ellipse, Line, Text } from 'react-konva';
+import { Stage, Layer, Line, Text } from 'react-konva';
 import Konva from 'konva';
 
 // -- local imports
@@ -31,6 +31,7 @@ import type {
 import useMockDispatcher from '@/dispatchers/useMockDispatcher';
 import useInaccessibleDispatcher from '@/dispatchers/useInaccessibleDispatcher';
 import useRectangleDispatcher from '@/dispatchers/useRectangleDispatcher';
+import useEllipseDispatcher from '@/dispatchers/useEllipseDispatcher';
 
 export interface CanvasProps {
   width: number;
@@ -41,101 +42,6 @@ export interface CanvasProps {
   currentTool: ToolChoice;
   disabled: boolean;
 }
-
-const useEllipseDispatcher = ({ shapeAttributes, addShapes }: OperationDispatcherProps): OperationDispatcher => {
-  const [mouseDownCoords, setMouseDownCoords] = useState<EventCoords | null>(null);
-  const [mouseCoords, setMouseCoords] = useState<EventCoords | null>(null);
-
-  const handlePointerDown = (ev: Konva.KonvaEventObject<MouseEvent>) => {
-    const { offsetX, offsetY } = ev.evt;
-
-    setMouseDownCoords({ x: offsetX, y: offsetY });
-    setMouseCoords({ x: offsetX, y: offsetY });
-  };
-
-  const handlePointerMove = (ev: Konva.KonvaEventObject<MouseEvent>) => {
-    const { offsetX, offsetY } = ev.evt;
-
-    setMouseCoords({ x: offsetX, y: offsetY });
-  };
-
-  const handlePointerUp = (ev: Konva.KonvaEventObject<MouseEvent>) => {
-    if (mouseDownCoords !== null) {
-      const { offsetX: xRelease, offsetY: yRelease } = ev.evt;
-      const { x: xOrigin, y: yOrigin } = mouseDownCoords;
-
-      addShapes([{
-        type: 'ellipse',
-        ...shapeAttributes,
-        x: xOrigin,
-        y: yOrigin,
-        radiusX: Math.abs(xRelease - xOrigin),
-        radiusY: Math.abs(yRelease - yOrigin),
-      }]);
-      setMouseDownCoords(null);
-    }
-  };
-
-  const getPreview = (): React.JSX.Element | null => {
-    if (mouseDownCoords && mouseCoords) {
-      const { x: xOrigin, y: yOrigin } = mouseDownCoords;
-      const { x: xCurr, y: yCurr } = mouseCoords;
-
-      return (
-        <Ellipse
-          x={xOrigin}
-          y={yOrigin}
-          radiusX={Math.abs(xCurr - xOrigin)}
-          radiusY={Math.abs(yCurr - yOrigin)}
-          fill="#ffaaaa"
-        />
-      );
-    } else {
-      return null;
-    }
-  };
-
-  const renderShape = (
-    key: string | number,
-    model: CanvasObjectModel
-  ): React.JSX.Element | null => {
-    if (model.type !== 'ellipse') {
-      return null;
-    } else {
-      const { x, y, radiusX, radiusY, fillColor, strokeColor, strokeWidth } = model;
-
-      return (
-        <Ellipse
-          key={key}
-          x={x}
-          y={y}
-          radiusX={radiusX}
-          radiusY={radiusY}
-          fill={fillColor}
-          stroke={strokeColor}
-          strokeWidth={strokeWidth}
-        />
-      );
-    }
-  };
-
-  const getTooltipText = () => {
-    if (mouseDownCoords) {
-      return 'Drag to desired shape, then release';
-    } else {
-      return 'Click to draw an ellipse';
-    }
-  };
-
-  return ({
-    handlePointerDown,
-    handlePointerMove,
-    handlePointerUp,
-    getPreview,
-    renderShape,
-    getTooltipText
-  });
-};// end useEllipseDispatcher
 
 const useVectorDispatcher = ({ shapeAttributes, addShapes }: OperationDispatcherProps): OperationDispatcher => {
   const [mouseDownCoords, setMouseDownCoords] = useState<EventCoords | null>(null);
---
+++ Frontend/src/dispatchers/useEllipseDispatcher.tsx
@@ -0,0 +1,120 @@
+// --- std imports
+import { useState } from 'react';
+
+// --- third-party imports
+import Konva from 'konva';
+import { Ellipse } from 'react-konva';
+
+// --- local imports
+import type {
+  OperationDispatcher,
+  OperationDispatcherProps
+} from '@/types/OperationDispatcher';
+import type {
+  CanvasObjectModel
+} from '@/types/CanvasObjectModel';
+import type {
+  EventCoords
+} from '@/types/EventCoords';
+
+// === useEllipseDispatcher ====================================================
+//
+// Tool for drawing ellipses.
+//
+// =============================================================================
+const useEllipseDispatcher = ({ shapeAttributes, addShapes }: OperationDispatcherProps): OperationDispatcher => {
+  const [mouseDownCoords, setMouseDownCoords] = useState<EventCoords | null>(null);
+  const [mouseCoords, setMouseCoords] = useState<EventCoords | null>(null);
+
+  const handlePointerDown = (ev: Konva.KonvaEventObject<MouseEvent>) => {
+    const { offsetX, offsetY } = ev.evt;
+
+    setMouseDownCoords({ x: offsetX, y: offsetY });
+    setMouseCoords({ x: offsetX, y: offsetY });
+  };
+
+  const handlePointerMove = (ev: Konva.KonvaEventObject<MouseEvent>) => {
+    const { offsetX, offsetY } = ev.evt;
+
+    setMouseCoords({ x: offsetX, y: offsetY });
+  };
+
+  const handlePointerUp = (ev: Konva.KonvaEventObject<MouseEvent>) => {
+    if (mouseDownCoords !== null) {
+      const { offsetX: xRelease, offsetY: yRelease } = ev.evt;
+      const { x: xOrigin, y: yOrigin } = mouseDownCoords;
+
+      addShapes([{
+        type: 'ellipse',
+        ...shapeAttributes,
+        x: xOrigin,
+        y: yOrigin,
+        radiusX: Math.abs(xRelease - xOrigin),
+        radiusY: Math.abs(yRelease - yOrigin),
+      }]);
+      setMouseDownCoords(null);
+    }
+  };
+
+  const getPreview = (): React.JSX.Element | null => {
+    if (mouseDownCoords && mouseCoords) {
+      const { x: xOrigin, y: yOrigin } = mouseDownCoords;
+      const { x: xCurr, y: yCurr } = mouseCoords;
+
+      return (
+        <Ellipse
+          x={xOrigin}
+          y={yOrigin}
+          radiusX={Math.abs(xCurr - xOrigin)}
+          radiusY={Math.abs(yCurr - yOrigin)}
+          fill="#ffaaaa"
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
+  const renderShape = (
+    key: string | number,
+    model: CanvasObjectModel
+  ): React.JSX.Element | null => {
+    if (model.type !== 'ellipse') {
+      return null;
+    } else {
+      const { x, y, radiusX, radiusY, fillColor, strokeColor, strokeWidth } = model;
+
+      return (
+        <Ellipse
+          key={key}
+          x={x}
+          y={y}
+          radiusX={radiusX}
+          radiusY={radiusY}
+          fill={fillColor}
+          stroke={strokeColor}
+          strokeWidth={strokeWidth}
+        />
+      );
+    }
+  };
+
+  const getTooltipText = () => {
+    if (mouseDownCoords) {
+      return 'Drag to desired shape, then release';
+    } else {
+      return 'Click to draw an ellipse';
+    }
+  };
+
+  return ({
+    handlePointerDown,
+    handlePointerMove,
+    handlePointerUp,
+    getPreview,
+    renderShape,
+    getTooltipText
+  });
+};// end useEllipseDispatcher
+
+export default useEllipseDispatcher;
