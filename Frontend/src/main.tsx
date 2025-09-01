abee79e877d0cea9eec8f433690d9f38a6d7da3a
Oliver A. Kelton <oakelton@gmail.com>
2025-09-01 15:00:17 -0700

    Created bare-bones redux store, wrapped App in provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import './index.css'
 import App from './App.tsx'
+import reduxStore from '@/app/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReduxProvider store={reduxStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
   </StrictMode>,
 )
