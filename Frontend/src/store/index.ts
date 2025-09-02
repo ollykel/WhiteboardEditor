3d31aa4e1b72c2e504d914d0a6059ba596ad1e0d
Oliver A. Kelton <oakelton@gmail.com>
2025-09-02 14:14:49 -0700

    Moved store to store/index.ts

--- Frontend/src/store/index.ts
+++ Frontend/src/store/index.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import rootReducer from '@/reducers/rootReducer';
+
 export default configureStore({
-  reducer: {},
-})
+  reducer: rootReducer
+});
