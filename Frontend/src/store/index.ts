e7188ff654a0f8a64d390fe9632de2705d55c0d3
Oliver A. Kelton <oakelton@gmail.com>
2025-09-02 14:19:35 -0700

    Re-implemented redux store without root reducer

--- Frontend/src/store/index.ts
+++ Frontend/src/store/index.ts
@@ -1,7 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import rootReducer from '@/reducers/rootReducer';
+import canvasObjectsReducer from './canvasObjects/canvasObjectsSlice';
+import canvasObjectsByCanvasReducer from './canvasObjects/canvasObjectsByCanvasSlice';
+import canvasesReducer from './canvases/canvasesSlice';
 
-export default configureStore({
-  reducer: rootReducer
+export const store = configureStore({
+  reducer: {
+    canvasObjects: canvasObjectsReducer,
+    canvasObjectsByCanvas: canvasObjectsByCanvasReducer,
+    canvases: canvasesReducer
+  }
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
