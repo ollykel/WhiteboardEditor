a8872cccef1aca960d4692aeac89ca2a1f10ba24
Oliver A. Kelton <oakelton@gmail.com>
2025-09-02 16:46:26 -0700

    Implemented noramlizeWhiteboard

---
+++ Frontend/src/store/whiteboards/whiteboardsNormalizers.ts
@@ -0,0 +1,60 @@
+import type {
+  CanvasData,
+  CanvasRecord,
+  WhiteboardData,
+  WhiteboardRecord
+} from '@/types/WebSocketProtocol';
+
+import {
+  normalizeCanvas,
+  type CanvasNormal
+} from '@/store/canvases/canvasesNormalizers';
+
+export interface WhiteboardNormal extends CanvasNormal {
+  whiteboards: WhiteboardRecord[];
+  canvasesByWhiteboard: Record<string, CanvasRecord[]>;
+}
+
+// === normalizeWhiteboard =====================================================
+//
+// Takes a complete canvas object and normalizes it so its constituent parts can
+// be stored.
+//
+// =============================================================================
+export const normalizeWhiteboard = (
+  whiteboard: WhiteboardData
+): WhiteboardNormal => {
+  const { id, name } = whiteboard;
+
+  const {
+    canvases,
+    canvasObjects,
+    canvasObjectsByCanvas
+  } = whiteboard.canvases
+    .map((canvas: CanvasData) => normalizeCanvas(id, canvas))
+    .reduce(
+    (accum: CanvasNormal, curr: CanvasNormal) => ({
+      canvases: [...accum.canvases, ...curr.canvases],
+      canvasObjects: [...accum.canvasObjects, ...curr.canvasObjects],
+      canvasObjectsByCanvas: ({
+        ...accum.canvasObjectsByCanvas,
+        ...curr.canvasObjectsByCanvas
+      })
+    }),
+    {
+      canvases: [],
+      canvasObjects: [],
+      canvasObjectsByCanvas: {}
+    }
+  );
+
+  return ({
+    whiteboards: [({ id, name })],
+    canvases,
+    canvasesByWhiteboard: {
+      [id]: canvases
+    },
+    canvasObjects,
+    canvasObjectsByCanvas
+  });
+};
