a9a7563546caf48b383dd3d9a7fc6f5a607ffaf9
Oliver A. Kelton <oakelton@gmail.com>
2025-09-02 14:42:51 -0700

    Created canvasesSelectors

---
+++ Frontend/src/store/canvases/canvasesSelectors.ts
@@ -0,0 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+import type {
+  RootState
+} from '@/store';
+
+import type {
+  CanvasKeyType
+} from '@/types/WebSocketProtocol';
+
+export const selectCanvasById = (state: RootState, canvasId: CanvasKeyType) => (
+  state.canvases[canvasId.toString()]
+);
+
+export const selectObjectsForCanvas = (state: RootState, canvasId: CanvasKeyType) => (
+  state.canvasObjectsByCanvas[canvasId.toString()]?.map(id => state.canvasObjects[id.toString()]) ?? []
+);
+
+export const selectCanvasWithObjects = createSelector(
+  [selectCanvasById, selectObjectsForCanvas],
+  (canvas, objects) => ({ ...canvas, shapes: objects })
+);
