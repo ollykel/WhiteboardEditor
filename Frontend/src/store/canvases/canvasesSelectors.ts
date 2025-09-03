180e9ffb541a13e4d40cd030c961db41fbd99816
Oliver A. Kelton <oakelton@gmail.com>
2025-09-02 20:10:02 -0700

    Implemented selectCanvasesByWhiteboardId

--- Frontend/src/store/canvases/canvasesSelectors.ts
+++ Frontend/src/store/canvases/canvasesSelectors.ts
@@ -8,8 +8,17 @@ import type {
   CanvasKeyType
 } from '@/types/WebSocketProtocol';
 
-export const selectCanvasById = (state: RootState, canvasId: CanvasKeyType) => (
-  state.canvases[canvasId.toString()]
+import type {
+  CanvasRecord,
+  WhiteboardIdType
+} from '@/types/WebSocketProtocol';
+
+export const selectCanvasById = (state: RootState, canvasId: CanvasKeyType): CanvasRecord | null => (
+  state.canvases[canvasId.toString()] || null
+);
+
+export const selectCanvasesByWhiteboardId = (state: RootState, whiteboardId: WhiteboardIdType): CanvasRecord[] => (
+  state.canvasesByWhiteboard[whiteboardId]?.map(canvasKey => state.canvases[canvasKey.toString()]) ?? []
 );
 
 export const selectObjectsForCanvas = (state: RootState, canvasId: CanvasKeyType) => (
@@ -20,3 +29,4 @@ export const selectCanvasWithObjects = createSelector(
   [selectCanvasById, selectObjectsForCanvas],
   (canvas, objects) => canvas ? ({ ...canvas, shapes: objects }) : null
 );
+
