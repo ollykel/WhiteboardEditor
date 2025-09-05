670c6b0ae076a0d2e66915b52acc8d01f39a6214
Joe Rogers <frogers@uci.edu>
2025-09-05 13:28:18 -0700

    Remove old canvas menu input component

--- Frontend/src/components/CreateCanvasInput.tsx
+++
@@ -1,7 +0,0 @@
-function CreateCanvasInput() {
-  return (
-    <div></div>
-  );
-}
-
-export default CreateCanvasInput;
--- Frontend/src/components/CreateCanvasMenu.tsx
+++ Frontend/src/components/CreateCanvasMenu.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 
-// import CreateCanvasInput from "./CreateCanvasInput";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
