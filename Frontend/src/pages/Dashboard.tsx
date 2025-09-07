2e6dc018ffd8a97056641144cca36d2b977584d5
Oliver A. Kelton <oakelton@gmail.com>
2025-09-06 22:29:11 -0700

    Refactored Dashboard to use arrow function syntax

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ import SharedWhiteboards from "@/components/SharedWhiteboards";
 import { useUser } from "@/hooks/useUser";
 import type { User } from "@/types/UserAuth";
 
-function Dashboard() {
+const Dashboard = (): React.JSX.Element => {
   const title: string = "<Whiteboard App>";
   const user: User = useUser().user;
   const navigate = useNavigate();
@@ -18,24 +18,26 @@ function Dashboard() {
   }
 
   return (
-    <div>
+    <>
       <HeaderAuthed
         title={title}
       />
 
-      <h1 className="text-4xl font-bold text-center">
-        Welcome Back, {user?.username}!
-      </h1>
-      <button
-        onClick={createNewWhiteboard}
-        className="flex flex-col items-center justify-center ml-10 mt-10 p-4 shadow rounded-lg hover:bg-gray-200 hover:cursor-pointer bg-stone-50"
-      >
-        + New Whiteboard
-      </button>
-      <YourWhiteboards />
-      <SharedWhiteboards />
-    </div>
+      <main>
+        <h1 className="text-4xl font-bold text-center">
+          Welcome Back, {user?.username}!
+        </h1>
+        <button
+          onClick={createNewWhiteboard}
+          className="flex flex-col items-center justify-center ml-10 mt-10 p-4 shadow rounded-lg hover:bg-gray-200 hover:cursor-pointer bg-stone-50"
+        >
+          + New Whiteboard
+        </button>
+        <YourWhiteboards />
+        <SharedWhiteboards />
+      </main>
+    </>
   );
-}
+};// end Dashboard
 
 export default Dashboard;
