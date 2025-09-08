5e219439e03ad4bd4fd7e876717cc851bd44bdb0
Oliver A. Kelton <oakelton@gmail.com>
2025-09-08 15:12:06 -0700

    Minimally working CreateWhiteboardModal

--- Frontend/src/components/CreateWhiteboardModal.tsx
+++ Frontend/src/components/CreateWhiteboardModal.tsx
@@ -30,18 +30,39 @@ import {
   Input
 } from '@/components/ui/input';
 
-const CreateWhiteboardModal = (): React.JSX.Element => {
+interface CreateWhiteboardFormAttribs {
+  name: string;
+}
+
+const FORM_ATTRIBS_DEFAULT = {
+  name: ''
+};
+
+export interface CreateWhiteboardFormData extends CreateWhiteboardFormAttribs {
+  collaboratorEmails: string[];
+}
+
+export interface CreateWhiteboardModalProps {
+  onSubmit: (data: CreateWhiteboardFormData) => void;
+}
+
+const CreateWhiteboardModal = ({
+  onSubmit
+}: CreateWhiteboardModalProps): React.JSX.Element => {
   // -- managed state
   const [isOpen, setIsOpen] = useState<boolean>();
   const [emailSet, setEmailSet] = useState<Record<string, boolean>>({});
   const [newEmail, setNewEmail] = useState<string>("");
+  const [formInputs, setFormInputs] = useState<CreateWhiteboardFormAttribs>({
+    ...FORM_ATTRIBS_DEFAULT
+  });
 
   const handleChangeNewEmail = (ev: React.ChangeEvent<HTMLInputElement>) => {
     ev.preventDefault();
     setNewEmail(ev.target.value);
   };
 
-  const handleAddNewEmail = (ev: React.FormEvent<HTMLFormElement>) => {
+  const handleAddNewEmail = (ev: React.MouseEvent<HTMLButtonElement>) => {
     ev.preventDefault();
 
     setNewEmail(newEmail => {
@@ -70,7 +91,7 @@ const CreateWhiteboardModal = (): React.JSX.Element => {
   const RemovableEmail = (email: string): React.JSX.Element => {
     return (
       <div
-        className="m-2 px-2 py-1 inline-block align-middle rounded-md bg-gray-200 border-gray-600"
+        className="mr-2 mb-2 px-2 py-1 inline-block align-middle rounded-2xl bg-gray-200 border-gray-600"
       >
         <button
           onClick={makeHandleRemoveEmail(email)}
@@ -85,6 +106,36 @@ const CreateWhiteboardModal = (): React.JSX.Element => {
     );
   };
 
+  const handleChangeInput = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    ev.preventDefault();
+
+    const name = ev.target.name
+    const value = ev.target.value;
+
+    setFormInputs(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    const data = ({
+      ...formInputs,
+      collaboratorEmails: Object.keys(emailSet)
+    });
+
+    if (! data.name) {
+      alert('Name required');
+    }
+
+    onSubmit(data);
+    setIsOpen(false);
+    setFormInputs({ ...FORM_ATTRIBS_DEFAULT });
+    setEmailSet({});
+  };
+
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
       <PopoverTrigger asChild>
@@ -97,49 +148,75 @@ const CreateWhiteboardModal = (): React.JSX.Element => {
       </PopoverTrigger>
 
       <PopoverContent className="w-200 p-0 m-4 flex flex-col flex-shrink">
-        <Command className="flex flex-col flex-shrink p-4">
-          <h1 className="text-lg font-semibold">
-            Invite users by email
-          </h1>
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-center text-2xl font-bold m-2">Create a new whiteboard</h2>
 
-          <form
-            onSubmit={handleAddNewEmail}
-            className="flex flex-row align-top w-160"
-          >
-            <Input
-              name="new-email"
-              type="email"
-              placeholder="Email"
-              onChange={handleChangeNewEmail}
-              value={newEmail}
-              className="mr-2"
-            />
+          <Command className="flex flex-col flex-shrink p-4">
+            <div className="flex flex-col">
+              <label htmlFor="whiteboard-name">Whiteboard Name:</label>
+              <Input
+                name="name"
+                type="text"
+                onChange={handleChangeInput}
+                value={formInputs.name}
+                required
+                placeholder="Whiteboard Name"
+              />
+            </div>
+          </Command>
 
+          <Command className="flex flex-col flex-shrink p-4">
+            <h3 className="text-lg font-semibold">
+              Invite collaborators by email
+            </h3>
+
+            <div
+              className="flex flex-row align-top w-160"
+            >
+              <Input
+                name="new-email"
+                type="email"
+                placeholder="Email"
+                onChange={handleChangeNewEmail}
+                value={newEmail}
+                className="mr-2"
+              />
+
+              <Button
+                variant="secondary"
+                onClick={handleAddNewEmail}
+              >
+                + Add Email
+              </Button>
+            </div>
+
+            <CommandGroup>
+              {/** Display user emails to add, with option to remove **/}
+              <h3>Collaborators to invite:</h3>
+              <ul className="flex flex-row flex-wrap">
+              {
+                  Object.keys(emailSet).length ?
+                    Object.keys(emailSet).map(email => (
+                        <li>
+                          {RemovableEmail(email)}
+                        </li>
+                      ))
+                    :
+                    "No emails selected"
+              }
+              </ul>
+            </CommandGroup>
+          </Command>
+
+          <div className="flex flex-row justify-center">
             <Button
               type="submit"
-              variant="secondary"
+              className="w-1/2"
             >
-              + Add Email
+              + Create Whiteboard
             </Button>
-          </form>
-
-          <CommandGroup>
-            {/** Display user emails to add, with option to remove **/}
-            <h2>Emails to invite:</h2>
-            <ul className="flex flex-row flex-wrap">
-            {
-                Object.keys(emailSet).length ?
-                  Object.keys(emailSet).map(email => (
-                      <li>
-                        {RemovableEmail(email)}
-                      </li>
-                    ))
-                  :
-                  "No emails selected"
-            }
-            </ul>
-          </CommandGroup>
-        </Command>
+          </div>
+        </form>
       </PopoverContent>
     </Popover>
   );
