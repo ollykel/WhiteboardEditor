6ce79e6df34a6175d2226cb014683f0012c6e272
Oliver A. Kelton <oakelton@gmail.com>
2025-09-18 18:02:22 -0700

    Updated WebSocketServer cargo tests to reflect new data types

--- WebSocketServer/src/lib.rs
+++ WebSocketServer/src/lib.rs
@@ -676,7 +676,7 @@ mod tests {
 
     #[tokio::test]
     async fn handle_client_message_create_shapes() {
-        let F64_PREC: f64 = 1.0e-16;
+        let f64_prec: f64 = 1.0e-16;
         let test_client_id = 0;
         let canvas_a_id = ObjectId::new();
         let shapes_expected = vec![
@@ -760,6 +760,8 @@ mod tests {
                             width: 512,
                             height: 512,
                             name: String::from("Canvas A"),
+                            time_created: Utc::now(),
+                            time_last_modified: Utc::now(),
                             shapes: HashMap::new(),
                             allowed_users: None, // None = open to all
                         }
@@ -781,7 +783,7 @@ mod tests {
                 ServerSocketMessage::CreateShapes { client_id, canvas_id, shapes } => {
                     if client_id != test_client_id {
                         panic!("Expected client_id = {}; got {}", test_client_id, client_id);
-                    } else if canvas_id != canvas_a_id {
+                    } else if canvas_id != canvas_a_id.to_string() {
                         panic!("Expected canvas_id = {}; got {}", canvas_a_id, canvas_id);
                     } else if shapes.len() != shapes_expected.len() {
                         panic!(
@@ -796,10 +798,10 @@ mod tests {
                         );
                     } else {
                         // success
-                        let mut shapes_entries : Vec<(&CanvasObjectIdType, &ShapeModel)> = shapes.iter()
+                        let mut shapes_entries : Vec<(&String, &ShapeModel)> = shapes.iter()
                             .collect();
 
-                        shapes_entries.sort_by_key(|(obj_id, _)| **obj_id);
+                        shapes_entries.sort_by_key(|(obj_id, _)| (*obj_id).clone());
 
                         for (ref shape_entry, ref shape_expected) in shapes_entries.iter().zip(shapes_expected.iter()) {
                             let (_, shape) = shape_entry;
@@ -809,19 +811,19 @@ mod tests {
                                     ShapeModel::Rect { x, y, width, height, stroke_width, stroke_color, fill_color },
                                     ShapeModel::Rect { x: x_exp, y: y_exp, width: width_exp, height: height_exp, stroke_width: stroke_width_exp, stroke_color: stroke_color_exp, fill_color: fill_color_exp }
                                 ) => {
-                                    if (x - x_exp).abs() > F64_PREC {
+                                    if (x - x_exp).abs() > f64_prec {
                                         panic!("Expected shape x = {}; got {}", x, x_exp);
                                     }
-                                    if (y - y_exp).abs() > F64_PREC {
+                                    if (y - y_exp).abs() > f64_prec {
                                         panic!("Expected shape y = {}; got {}", y, y_exp);
                                     }
-                                    if (width - width_exp).abs() > F64_PREC {
+                                    if (width - width_exp).abs() > f64_prec {
                                         panic!("Expected shape width = {}; got {}", width, width_exp);
                                     }
-                                    if (height - height_exp).abs() > F64_PREC {
+                                    if (height - height_exp).abs() > f64_prec {
                                         panic!("Expected shape height = {}; got {}", height, height_exp);
                                     }
-                                    if (stroke_width - stroke_width_exp).abs() > F64_PREC {
+                                    if (stroke_width - stroke_width_exp).abs() > f64_prec {
                                         panic!("Expected shape stroke_width = {}; got {}", stroke_width, stroke_width_exp);
                                     }
                                     if stroke_color != stroke_color_exp {
