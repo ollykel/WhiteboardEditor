8473deb3183e8925b003e1f1893de5c42745a9cf
Oliver A. Kelton <oakelton@gmail.com>
2025-09-21 00:23:27 -0700

    Added explanatory comments to match in Frontend/src/utils.ts

--- Frontend/src/utils.ts
+++ Frontend/src/utils.ts
@@ -6,10 +6,18 @@
 
 // === match ===================================================================
 //
+// Takes as arguments an object which maps keys to functions which each take
+// no arguments and return elements of the same type, plus a key corresponding
+// to one of the keys in the object. Calls the function indicated by the key and
+// returns the result. If key does not exist in the object, throws an error.
 //
+// Imitates match syntax (tagged union handling) in Rust.
 //
 // =============================================================================
-export const match = <KeyType extends string | number, ReturnType> (matchArms: Record<KeyType, () => ReturnType>, key: KeyType): ReturnType => {
+export const match = <KeyType extends string | number, ReturnType> (
+  matchArms: Record<KeyType, () => ReturnType>,
+  key: KeyType
+): ReturnType => {
   if (! (key in matchArms)) {
     throw new Error(`Key ${key} not handled by match arm`);
   } else {
