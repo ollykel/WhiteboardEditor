13ad52399f9651c4fa09b71c6407732b9654eeb9
Joe Rogers <frogers@uci.edu>
2025-09-22 23:36:23 -0700

    Implement basic useTextDispatcher to create a textbox

---
+++ Frontend/src/dispatchers/useTextDispatcher.tsx
@@ -0,0 +1,122 @@
+import React, {
+  useState
+} from 'react';
+
+import Konva from 'konva';
+import { Text } from 'react-konva';
+
+import type {
+  OperationDispatcher,
+  OperationDispatcherProps
+} from '@/types/OperationDispatcher';
+import type {
+  CanvasObjectModel
+} from '@/types/CanvasObjectModel';
+import type {
+  EventCoords
+} from '@/types/EventCoords';
+
+// === useTextDispatcher ==================================================
+//
+// Tool for writing text.
+//
+// =============================================================================
+const useTextDispatcher = ({
+  shapeAttributes,
+  addShapes,
+}: OperationDispatcherProps
+): OperationDispatcher => {
+  const [mouseDownCoords, setMouseDownCoords] = useState<EventCoords | null>(null);
+
+  const handlePointerDown = (ev: Konva.KonvaEventObject<MouseEvent>) => {
+    const { offsetX, offsetY } = ev.evt;
+
+    setMouseDownCoords({ x: offsetX, y: offsetY }); 
+  };
+
+  const handlePointerMove = (_ev: Konva.KonvaEventObject<MouseEvent>) => {
+    // Nothing to do
+  };
+
+  const handlePointerUp = (ev: Konva.KonvaEventObject<MouseEvent>) => {
+    if (mouseDownCoords !== null) {
+      const { offsetX: xA, offsetY: yA } = ev.evt;
+      const { x: xB, y: yB } = mouseDownCoords;
+      const xMin = Math.min(xA, xB);
+      const yMin = Math.min(yA, yB);
+      const width = Math.abs(xA - xB);
+      const height = Math.abs(yA - yB);
+
+      addShapes([{
+        type: 'text',
+        ...shapeAttributes,
+        fontSize: 20,
+        color: 'red',
+        x: xMin,
+        y: yMin,
+        width,
+        height,
+      }]);
+      setMouseDownCoords(null);
+    }
+  };
+
+  const getPreview = () => null;
+
+  const renderShape = (
+    _key: string | number,
+    model: CanvasObjectModel,
+    isDraggable: boolean,
+    // handleUpdateShapes: (shapes: Record<CanvasObjectIdType, CanvasObjectModel>) => void
+  ): React.JSX.Element | null => {
+    if (model.type !== 'text') {
+      return null;
+    } else {
+      const {
+        fontSize,
+        color,
+        x,
+        y,
+        width,
+        height
+      } = model;
+
+      return (
+        <Text
+          text={"Example Text"}
+          fontSize={fontSize}
+          color={color}
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          draggable={isDraggable}
+          // onDblClick={handleTextDblClick}
+          // onDblTap={handleTextDblClick}
+          // onTransform={handleTransform}
+          // visible={!isEditing}
+        />
+      )
+    }
+  };
+
+  const getTooltipText = () => {
+    if (mouseDownCoords) {
+      return 'Drag to desired textbox size, then release';
+    } else {
+      return 'Click to draw a textbox';
+    }
+  };  
+
+  return ({
+    handlePointerDown,
+    handlePointerMove,
+    handlePointerUp,
+    getPreview,
+    renderShape,
+    // handleUpdateShapes,
+    getTooltipText,
+  });
+}
+
+export default useTextDispatcher;
