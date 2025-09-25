334f5cf8cd24acbac48eba9b02af4fa1a43a9997
Joe Rogers <frogers@uci.edu>
2025-09-24 20:54:21 -0700

    Initialize TextEditor fields

--- Frontend/src/components/TextEditor.tsx
+++ Frontend/src/components/TextEditor.tsx
@@ -12,6 +12,21 @@ interface TextEditorProps {
 const TextEditor = ({ textNode, onClose, onChange }: TextEditorProps) => {
   const textareaRef = useRef(null);
 
+  if (!textareaRef.current) {
+    return;
+  }
+
+  const textarea = textareaRef.current;
+  const stage = textNode.getStage();
+  const textPosition = textNode.position();
+  const stageBox = stage?.container().getBoundingClientRect();
+  const areaPosition = {
+    x: textPosition.x,
+    y: textPosition.y,
+  }
+
+  
+
   return (
     <Html>
       <textarea 
