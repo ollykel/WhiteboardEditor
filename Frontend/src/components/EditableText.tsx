46384b99174e38981521e020b67b8150c01ffc5c
Joe Rogers <frogers@uci.edu>
2025-09-26 21:13:50 -0700

    Add logic for deselecting when clicking outside of textbox

--- Frontend/src/components/EditableText.tsx
+++ Frontend/src/components/EditableText.tsx
@@ -29,16 +29,37 @@ const EditableText = ({
   draggable,
 }: EditableTextProps) => {
   const [text, setText] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
   const [isEditing, setIsEditing] = useState(true);
   const [textWidth, setTextWidth] = useState(width);
-  const textRef = useRef<Konva.Text | null>(null);
-  const trRef = useRef<Konva.Transformer | null>(null);
 
+  const textRef = useRef<Konva.Text>(null);
+  const trRef = useRef<Konva.Transformer>(null);
+
+  // attach Transformer for editing when selected
   useEffect(() => {
-    if (trRef.current && textRef.current) {
+    if (isSelected && trRef.current && textRef.current) {
       trRef.current.nodes([textRef.current]);
+      trRef.current.getLayer()?.batchDraw();
     }
-  }, [isEditing])
+  }, [isSelected])
+
+  // deslect when clicking outside
+  useEffect(() => {
+    const handleStageClick = (e: Konva.KonvaEventObject<MouseEvent>) => {
+      if (e.target === e.target.getStage()) {
+        setIsEditing(false);
+        setIsSelected(false);
+      }
+    };
+
+    const stage = textRef.current?.getStage();
+    stage?.on("click", handleStageClick);
+
+    return () => {
+      stage?.off("click", handleStageClick);
+    };
+  }, []);
 
   const handleTextDblClick = useCallback((): void => {
     setIsEditing(true);
@@ -85,7 +106,7 @@ const EditableText = ({
           onClose={() => setIsEditing(false)}
         />
       )} 
-      {!isEditing && (
+      {isSelected && (
         <Transformer
           ref={trRef}
           boundBoxFunc={(_oldBox, newBox) => ({
