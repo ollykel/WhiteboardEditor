7db5e4b5e16676aaa2a09126264cbd0456a416fc
Oliver A. Kelton <oakelton@gmail.com>
2025-10-13 20:42:59 -0700

    Created app.config.ts in Frontend to store app-wide constants.
    Set APP_NAME to "Boardly" in app.config.ts.

---
+++ Frontend/src/app.config.ts
@@ -0,0 +1,7 @@
+// === app.config.ts ===========================================================
+//
+// Contains runtime constants that need to be accessed by multiple components.
+//
+// =============================================================================
+
+export const APP_NAME = "Boardly";
