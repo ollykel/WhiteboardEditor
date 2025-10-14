c4ba16e563115bdafd71b657c280a0f3ba91a181
Oliver A. Kelton <oakelton@gmail.com>
2025-10-13 21:12:26 -0700

    Added some basic landing page info to the UserAuth page.

--- Frontend/src/pages/UserAuth.tsx
+++ Frontend/src/pages/UserAuth.tsx
@@ -13,16 +13,48 @@ interface UserAuthProps {
 function UserAuth({ action }: UserAuthProps) {
   const title = APP_NAME;
   const pageTitle = `${title} | ${APP_NAME}`;
+  let authActionLabel : string;
+
+  switch (action) {
+    case 'login':
+      authActionLabel = 'Log in';
+      break;
+    case 'signup':
+      authActionLabel = 'Create an Account';
+      break;
+    default:
+      throw new Error(`unrecognized auth action: ${action}`);
+  }// -- end switch (action)
 
   return (
     <Page
       title={pageTitle}
     >
       <HeaderUnauthed
-        title={title}
+        title={authActionLabel}
       />
 
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        {/** Main branding section **/}
+        <div
+          id="branding"
+          className="text-center mx-56 my-16"
+        >
+          <h1 className="text-8xl font-light my-8">
+            {APP_NAME}
+          </h1>
+
+          <h2 className="text-4xl font-thin my-4">
+            The better web whiteboard
+          </h2>
+
+          <p className="text-lg font-sans">
+            Need a place to make quick and easy diagrams to share with your colleagues? Look no further — Boardly is here for you.
+            Get started in minutes and share your designs with your peers with a simple email invite.
+          </p>
+        </div>
+
+        {/** Auth portal **/}
         <div className="rounded-lg shadow-md bg-stone-50 p-10">
           <AuthForm initialAction={action}/>
         </div>
