fdb57e0d8af1f9b21aee4e9f5f452f211ea26c07
Joe Rogers <frogers@uci.edu>
2025-11-09 12:41:17 -0800

    Create new utility function for capturing image of canvas, to be used in download and thumbnail capture

---
+++ Frontend/src/lib/captureImage.ts
@@ -0,0 +1,52 @@
+import type Konva from "konva";
+
+import { 
+  type RefObject, 
+} from "react";
+
+import {
+  KONVA_NODE_UI_ONLY_KEY,
+} from '@/app.config';
+
+import type { CanvasIdType } from "@/types/WebSocketProtocol";
+
+export const captureImage = (canvasGroupRefsByIdRef: RefObject<Record<CanvasIdType, RefObject<Konva.Group | null>>>,canvasId: string): string => {
+  const canvasGroupRef : RefObject<Konva.Group | null> | undefined = canvasGroupRefsByIdRef.current[canvasId];
+  
+  if (! canvasGroupRef?.current) {
+    console.error('Could not find ref to Canvas with id', canvasId);
+    alert('Error exporting Canvas');
+
+    return "";
+  } else {
+    // -- create a clone of the Canvas group that excludes UI-only elements
+    //    such as borders and tooltips.
+    const exportableCanvas : Konva.Node = canvasGroupRef.current.clone();
+
+    const isNodeContainer = (node: Konva.Node): node is Konva.Container => node.hasChildren();
+
+    const destroyUIOnlyDescendants = (node: Konva.Node) => {
+      if (isNodeContainer(node)) {
+        for (const child of node.getChildren()) {
+          if (child.hasName(KONVA_NODE_UI_ONLY_KEY)) {
+            child.destroy();
+          } else {
+            destroyUIOnlyDescendants(child);
+          }
+        }// -- end for child
+      }
+    };// -- end destroyUIOnlyDescendants
+
+    // -- filter out UI-only nodes
+    destroyUIOnlyDescendants(exportableCanvas);
+
+    const exportUrl : string = exportableCanvas.toDataURL({
+      mimeType: 'image/png',
+    });
+
+    // -- destroy temporary exportable canvas node
+    exportableCanvas.destroy(); 
+
+    return exportUrl;
+  }
+}
