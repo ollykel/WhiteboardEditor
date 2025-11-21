5f51999181079c3edf7509aec1425a34c2b577b0
Joe Rogers <fjrogers@paciolan.com>
2025-11-20 22:36:39 -0800

    Remove the updateWhiteboardThumbnail service, it's not possible to do this from the backend without current react state

--- RestAPI/src/services/whiteboardService.ts
+++ RestAPI/src/services/whiteboardService.ts
@@ -244,17 +244,3 @@ export const setSharedUsers = async (
     };
   }
 };// -- end setSharedUsers
-
-export const updateWhiteboardThumbnail = async (
-  whiteboardId: string,
-  thumbnailUrl: string,
-) => {
-  const whiteboard = await Whiteboard.findById(whiteboardId);
-
-  if (!whiteboard) return null;
-
-  whiteboard.thumbnail_url = thumbnailUrl;
-  await whiteboard.save();
-
-  return whiteboard.toAttribView();
-}
