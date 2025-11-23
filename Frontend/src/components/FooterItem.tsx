269573456b007bfcfbb3dfe0f4568b6044eb66e3
Joe Rogers <fjrogers@paciolan.com>
2025-11-22 19:48:21 -0800

    Update font formatting

--- Frontend/src/components/FooterItem.tsx
+++ Frontend/src/components/FooterItem.tsx
@@ -16,15 +16,15 @@ const FooterItem = ({
 	type,
 }: FooterItemType) => {
 	const variants = {
-		header: "flex items-center gap-4 p-2 font-bold text-lg",
-		standard: "flex items-center gap-4 p-2 hover:opacity-80",
+		header: "font-bold text-lg text-h2-text",
+		standard: "text-md text-h2-text",
 		footnote: "text-xs text-gray-500"
 	};
 
 	const imageSize = type === "header" ? 24 : 18;
 
 	return (		
-		<a href={href} className={variants[type]}>
+		<a href={href} className={"flex items-center gap-4 p-2 hover:opacity-80 " + variants[type]}>
 			{icon && <img src={icon} width={imageSize} height={imageSize} alt={label + " icon"} />}
 			{label}
 		</a>
--- Frontend/src/components/FooterSection.tsx
+++ Frontend/src/components/FooterSection.tsx
@@ -14,7 +14,7 @@ const FooterSection = ({
 }: FooterSectionType) => {
 	return (
 		<div className="flex flex-col place-items-center p-8 px-20">
-			{title && <h4 className="font-semibold mb-2 p-2">{title}</h4>}
+			{title && <h4 className="font-semibold text-h1-text mb-2 p-2">{title}</h4>}
 			<ul className="space-y-2">
 				{items.map((item) => (
 					<FooterItem key={item.label} {...item} />
