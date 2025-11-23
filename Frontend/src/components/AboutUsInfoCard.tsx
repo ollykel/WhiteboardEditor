9a061abcb7c746db45373bda1e840bb918e823ef
Joe Rogers <fjrogers@paciolan.com>
2025-11-22 23:52:02 -0800

    Format the info cards for responsiveness

--- Frontend/src/components/AboutUsInfoCard.tsx
+++ Frontend/src/components/AboutUsInfoCard.tsx
@@ -16,18 +16,40 @@ const AboutUsInfoCard = ({
   gitHubLink,
 }: AboutUsInfoCardProps) => {
   return (
-    <div className="flex">
-      <img src={imgSrc} alt={`${name}'s Photo`} />
-      <div className="flex flex-col">
-        <h3>{name}</h3>
-        <div className="flex">
+    <div className="flex flex-col md:flex-row">
+      <img 
+        src={imgSrc} 
+        alt={`${name}'s Photo`}
+        width={240}
+        height={240}
+        className="rounded-full shadow-2xl m-4"
+      />
+      <div className="flex flex-col justify-center pb-12 mx-2">
+        <h3 className="text-center font-bold text-2xl p-4">{name}</h3>
+        <div className="flex flex-col xl:flex-row items-center">
           <a href={linkedInLink} target="_blank">
-            <img src={linkedInImgSrc} alt={`${name}'s LinkedIn`} />
-            LinkedIn
+            <div className="flex gap-2 m-2 items-center">
+              <img 
+                src={linkedInImgSrc} 
+                alt={`${name}'s LinkedIn`} 
+                width={50}
+                height={50}
+                className="shadow-2xl"
+              />
+              <p className="font-bold">LinkedIn</p>
+            </div>
           </a>
           <a href={gitHubLink} target="_blank">
-            <img src={gitHubImgSrc} alt={`${name}'s GitHub`} />
-            GitHub
+            <div className="flex gap-2 m-2 items-center">
+              <img 
+                src={gitHubImgSrc} 
+                alt={`${name}'s GitHub`} 
+                width={50}
+                height={50}
+                className="shadow-2xl"
+              />
+              <p className="font-bold">GitHub</p>
+            </div>
           </a>
         </div>
       </div>
