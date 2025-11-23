62783c1d883a518bc2ec0984836e500bd1012144
Joe Rogers <fjrogers@paciolan.com>
2025-11-22 23:16:36 -0800

    Create AboutUs info card to hold our personal info

---
+++ Frontend/src/components/AboutUsInfoCard.tsx
@@ -0,0 +1,38 @@
+interface AboutUsInfoCardProps {
+  name: string;
+  imgSrc: string;
+  linkedInImgSrc: string;
+  linkedInLink: string;
+  gitHubImgSrc: string;
+  gitHubLink: string;
+}
+
+const AboutUsInfoCard = ({
+  name,
+  imgSrc,
+  linkedInImgSrc,
+  linkedInLink,
+  gitHubImgSrc,
+  gitHubLink,
+}: AboutUsInfoCardProps) => {
+  return (
+    <div className="flex">
+      <img src={imgSrc} alt={`${name}'s Photo`} />
+      <div className="flex flex-col">
+        <h3>{name}</h3>
+        <div className="flex">
+          <a href={linkedInLink}>
+            <img src={linkedInImgSrc} alt={`${name}'s LinkedIn`} />
+            LinkedIn
+          </a>
+          <a href={gitHubLink}>
+            <img src={gitHubImgSrc} alt={`${name}'s GitHub`} />
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUsInfoCard;
